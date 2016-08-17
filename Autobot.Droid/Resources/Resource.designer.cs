55d859c8d54fdb2b3385c8cf1c5379e24eb3da63
Jawahar <jawahars_16@live.in>
2016-08-17 06:48:48 +0530

    no message

--- Autobot.Droid/Resources/Resource.designer.cs
+++ Autobot.Droid/Resources/Resource.designer.cs
@@ -289,11 +289,11 @@ namespace Autobot.Droid
 			// aapt resource value: 0x7f070001
 			public const int MvxBindingTagUnique = 2131165185;
 			
-			// aapt resource value: 0x7f070009
-			public const int fab = 2131165193;
+			// aapt resource value: 0x7f07000c
+			public const int fab = 2131165196;
 			
-			// aapt resource value: 0x7f07000a
-			public const int icon = 2131165194;
+			// aapt resource value: 0x7f07000d
+			public const int icon = 2131165197;
 			
 			// aapt resource value: 0x7f070006
 			public const int linearLayout1 = 2131165190;
@@ -310,17 +310,26 @@ namespace Autobot.Droid
 			// aapt resource value: 0x7f070005
 			public const int promptList = 2131165189;
 			
-			// aapt resource value: 0x7f070008
-			public const int selectTriggerBtn = 2131165192;
+			// aapt resource value: 0x7f07000b
+			public const int selectTriggerBtn = 2131165195;
 			
-			// aapt resource value: 0x7f07000c
-			public const int textView = 2131165196;
+			// aapt resource value: 0x7f07000f
+			public const int textView = 2131165199;
 			
 			// aapt resource value: 0x7f070007
 			public const int textView1 = 2131165191;
 			
-			// aapt resource value: 0x7f07000b
-			public const int title = 2131165195;
+			// aapt resource value: 0x7f070009
+			public const int textView2 = 2131165193;
+			
+			// aapt resource value: 0x7f07000a
+			public const int textView3 = 2131165194;
+			
+			// aapt resource value: 0x7f07000e
+			public const int title = 2131165198;
+			
+			// aapt resource value: 0x7f070008
+			public const int view1 = 2131165192;
 			
 			static Id()
 			{
